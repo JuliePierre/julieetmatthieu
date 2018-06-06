295d7bf0242fab52ca3dbb7870a365aa7ea1e64a
Julie Pierre <julie.pierre3@gmail.com>
2018-06-06 20:11:33 +0200

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'infos-pratiques', to: "pages#infos"
+  get 'infos-pratiques', to: "pages#infos", as: :infos
 
   get 'components', to: 'pages#components'
   get 'landing', to: 'pages#landing'
