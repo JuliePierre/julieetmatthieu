4201f3e01345f20178ae2c763536216ad7fe8e40
Julie Pierre <julie.pierre3@gmail.com>
2018-06-06 19:48:41 +0200

    page infos complémentaires ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,20 @@ class PagesController < ApplicationController
 
   def landing
   end
+
+  def infos
+    @markers = [
+      {lat: 47.264302, lng: -2.340186, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Mairie" })}},
+      {lat: 47.243593, lng: -2.312504, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Ker Yette" })}},
+      {lat: 47.270689, lng: -2.344823, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Gare de Pornichet" })}},
+      {lat: 47.286644, lng: -2.210957, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Gare de Saint-Nazaire" })}},
+      {lat: 47.256408, lng: -2.338274, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Hotel du Chateau des Tourelles" })}},
+      {lat: 47.248144, lng: -2.311708, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Hotel Azureva" })}},
+      {lat: 47.238381, lng: -2.279730, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Hotel de la Plage Saint Marc" })}},
+      {lat: 47.255639, lng: -2.338253, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Hotel Le Belouga" })}},
+      {lat: 47.260963, lng: -2.339944, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Hotel Oceania" })}},
+      {lat: 47.265601, lng: -2.344373, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Hotel Ibis" })}},
+      {lat: 47.250448, lng: -2.321870, infoWindow: { content: render_to_string(partial: "/pages/map_box", locals: { lieu: "Camping Bel air" })}}
+    ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'infos-pratiques', to: "pages#infos"
 
   get 'components', to: 'pages#components'
   get 'landing', to: 'pages#landing'
